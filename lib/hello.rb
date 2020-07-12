a91ec0561b357b6c628a730661aee050263fbf51
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-12 17:50:41 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,19 +1,19 @@
 def hello_t(array)
   if block_given?
-  i = 0
+    i = 0
   
   while i < array.length
   yield (array[i])
     i = i +1  
-end
-
-array
+    end
+    
+    array
 end
 
 # call your method here!
 ["Tim", "Tom", "Jim"]
 hello_t(["Tim", "Tom", "Jim"]) do |name|
   if name.start_with? ("T")
-    puts "Hello, #{name}"
+    puts "Hi, #{name}"
   end
 end
